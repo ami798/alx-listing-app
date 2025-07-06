75b78e24ac497a04305a74ae9532817679a5449b
Amira Abdurahman <amiraabdurahman8@gmail.com>
2025-07-06 19:49:04 +0300

    addition

--- constants/index.ts
+++
@@ -1,6 +0,0 @@
-export const API_BASE_URL = 'https://api.example.com'
-
-export const UI_TEXT = {
-  BOOK_NOW: 'Book Now',
-  DETAILS: 'Details',
-}
--- interfaces/index.ts
+++
@@ -1,9 +0,0 @@
-export interface CardProps {
-  title: string
-  image: string
-}
-
-export interface ButtonProps {
-  label: string
-  onClick: () => void
-}
